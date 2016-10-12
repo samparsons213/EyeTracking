0d5657d347a4daa0032b8619af07ccb254bde423
Sam <samuel.parsons.09@ucl.ac.uk>
2016-10-12 15:23:16 +0100

    process_file_script.m was a scripted precursor to the function loadOneFile.m. The amendments in this commit relate to allowing for one file to contain tracks for one or both eyes, and take account of the blinks, as per SBLINK/EBLINK, accordingly. Now superceded by loadOneFile.m

--- process_file_script.m
+++ process_file_script.m
@@ -2,17 +2,45 @@
 
 % Preliminaries
 file_folder = 'C:\Users\Sam\OneDrive\C-PLACID\Eye Tracking Data\';
-file_name = 'armgr.asc';
-first_data_line = 138;
+% % file_name = 'armgr.asc'; % Control patient
+% % first_data_line = 138; % Specific to armgr.asc
+% % % % file_name = 'conpa.asc'; % PCA patient
+% % % % first_data_line = 36; % Specific to conpa.asc
+file_name = 'biged.asc'; % Control patient
+first_data_line = 91; % Specific to biged.asc
+
+n_eyes = 1; % Specific to some data files. Number of eye coordinates in
+%             each track - either one (left or right) or two (left and 
+%             right)
+
+% % n_eyes = 2; % Specific to some data files
+
+eye_lr = 'L'; % Specific to some data files
+
+% % eye_lr = 'R'; % Specific to some data files
+
+eye_track_line_length = 2 + 3*n_eyes; % timestamp, final full stop, and
+%                                       x,y,dilation for each eye 
+%                                       coordinate per track
+eye_track_x = 2 + (eye_lr == 'R') * (n_eyes-1)*3; % if both eyes are
+%                                                   tracked then left eye 
+%                                                   is recorded first, so 
+%                                                   coordinates on line 
+%                                                   will be shifted 3 
+%                                                   places to the right for
+%                                                   right eye when 
+%                                                   n_eyes = 2
 
 % Open file, read line by line. If line is an eye track then append to eye
 % tracking data, if line is a target placement then place in target data,
 % otherwise skip line. Line is determined to be an eye track if its length
-% after splitting on ' ' is 5 (timestamp, x, y, dilation, .), and the
-% final element is a '.'. Line is determined to be a target placement if
-% length after splitting on ' ' is 10, and the fifth element is
-% 'TARET_POS'. If a SBLINK (start blink) line is read, don't read eye
-% tracks until an EBLINK (end blink) line is read.
+% after splitting on ' ' is eye_track_line_length (timestamp, x, y,
+% dilation, . - see above for two eye equiivalent), and the final element
+% is a '.'. Line is determined to be a target placement if length after
+% splitting on ' ' is 10, and the fifth element is 'TARET_POS'. If a SBLINK
+% L/R (start blink) line is read, don't read eye tracks until an EBLINK L/R
+% (end blink) line is read. Only eye blinks for one eye are considered as
+% only one eye is being analysed right now.
 
 % % cd(file_folder)
 fileID = fopen([file_folder, file_name]);
@@ -61,13 +89,13 @@ while ~feof(fileID)
     if strcmp(new_line{1}, 'END')
         experiment_idx = experiment_idx + 1;
     end
-    if strcmp(new_line{1}, 'SBLINK')
+    if strcmp(new_line{1}, 'SBLINK') && strcmp(new_line{2}, eye_lr)
         add_eye_track = false;
     end
-    if strcmp(new_line{1}, 'EBLINK')
+    if strcmp(new_line{1}, 'EBLINK') && strcmp(new_line{2}, eye_lr)
         add_eye_track = true;
     end
-    if (length(new_line) == 5) && strcmp(new_line{end}, '.') && add_eye_track
+    if (length(new_line) == eye_track_line_length) && strcmp(new_line{end}, '.') && add_eye_track
         n_eye_tracks(experiment_idx) = n_eye_tracks(experiment_idx) + 1;
     end
     if (length(new_line) == 10) && strcmp(new_line{5}, 'TARGET_POS')
@@ -107,20 +135,20 @@ while ~feof(fileID)
         eye_track_row_idx = 0;
         target_placing_row_idx = 0;
     end
-    if strcmp(new_line{1}, 'SBLINK')
+    if strcmp(new_line{1}, 'SBLINK') && strcmp(new_line{2}, eye_lr)
         add_eye_track = false;
     end
-    if strcmp(new_line{1}, 'EBLINK')
+    if strcmp(new_line{1}, 'EBLINK') && strcmp(new_line{2}, eye_lr)
         add_eye_track = true;
     end
-    if (length(new_line) == 5) && strcmp(new_line{end}, '.') && add_eye_track
+    if (length(new_line) == eye_track_line_length) && strcmp(new_line{end}, '.') && add_eye_track
         eye_track_row_idx = eye_track_row_idx + 1;
         eye_tracking{experiment_idx, 1}(eye_track_row_idx) =...
             str2double(new_line{1});
         eye_tracking{experiment_idx, 2}(eye_track_row_idx, 1) =...
-            str2double(new_line{2});
+            str2double(new_line{eye_track_x});
         eye_tracking{experiment_idx, 2}(eye_track_row_idx, 2) =...
-            str2double(new_line{3});
+            str2double(new_line{eye_track_x+1});
     end
     if (length(new_line) == 10) && strcmp(new_line{5}, 'TARGET_POS')
         target_placing_row_idx = target_placing_row_idx + 1;
