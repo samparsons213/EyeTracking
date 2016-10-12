9e459b9f6533503fc0703fd41e0a8c840d99e825
Sam <samuel.parsons.09@ucl.ac.uk>
2016-10-12 12:52:45 +0100

    changed generateRandomParameters.m so emission_means and emission_covs are drawn from data dependent distributions. For each latent direction, the subset of transformed observations that have a true in zero_prob are used as a basis for each distribution. For emission_means, the means and covariance matrices of these subsets are used as parameters in a multivariate Gaussian to generate each vector in emission_means. For emission_covs, the covariance matrices of these subsets are taken to be the mean matrix for the Wishart_2(2, V) distribution i.e. V is constructed so the expected value is correct. Covariance matrices are then drawn from these Wisharts.

--- generateRandomParameters.m
+++ generateRandomParameters.m
@@ -1,20 +1,31 @@
 function [pi_1, P, emission_means, emission_covs] =...
-    generateRandomParameters(dim_x, alpha)
+    generateRandomParameters(y, zero_probs, alpha)
 % Returns randomly initialised parameters for the proposed HMM, taking one
 % hyperparameter as input for producing random probability distributions.
-% Random covariance matrices are generated as Wishart_2(2, I), thereby
-% having E[emission_covs(:, :, i)] = 2I for all i
+% emission_means and emission_covs are drawn from distributions depending
+% on y and zero_probs, for details read below
 
 % Inputs:
 
-% dim_x:            positive integer at least 2, giving the number of
-%                   latent states, those being 'no movement' and each of m
-%                   canonical latent directions
+% y:                either a [n 2 m+1] array of transformed difference data
+%                   for one person and one experiment, or an [n_people 1]
+%                   cell array, each element of which is a [n 2 m+1] array
+%                   of transformed difference data, for different n but
+%                   constant m
+
+% zero_probs:       either a [n 2 m] binary array where zeros indicate zero
+%                   posterior probability for that latent state at that
+%                   time, and ones indicate positive posterior probability,
+%                   or a [n_people 1] cell array, each element of which is
+%                   such a binary array with n equal to the corresponding
+%                   n in y and constant m, or a logical array of that size
+%                   with false <=> 0 and true <=> 1
 
 % alpha:            positive real number giving the single Dirichlet
 %                   parameter for all randomly drawn distributions, i.e. if
 %                   p is a randomly drawn 1 by n distribution it is sampled
-%                   from D(alpha*ones(1, n))
+%                   from D(alpha*ones(1, n)). If not input then it is set
+%                   to its default value of 1
 
 % Outputs:
 
@@ -29,23 +40,70 @@ function [pi_1, P, emission_means, emission_covs] =...
 
 % Author:           Sam Parsons
 % Date created:     23/09/2016
-% Last amended:     23/09/2016
+% Last amended:     29/09/2016
 
 %     *********************************************************************
 %     Check inputs
 %     *********************************************************************
 
-%     dim_x must be input
-    if nargin < 1
-        error('dim_x must be input')
+%     y and zero_probs must be input
+    if nargin < 2
+        error('y and zero_probs must be input')
     end
-%     dim_x must be a positive integer at least 2
-    if ~(isnumeric(dim_x) && isreal(dim_x) && isscalar(dim_x) &&...
-            (dim_x == round(dim_x)) && (dim_x >= 2))
-        error('dim_x must be a positive integer at least 2')
+%     y must be either a [n 2 m+1] real array, or a [n_people 1] cell
+%     array, each element of which is [n 2 m+1] for some m and constant m
+    if ~( (iscell(y) && iscolumn(y)) ||...
+            (isnumeric(y) && isreal(y) && (ndims(y) == 3) && (size(y, 2) == 2)) )
+        err_msg = ['y must either be a [n 2 m+1] reall array, or a',...
+            ' [n_people 1] cell array'];
+        error(err_msg)
+    end
+    if iscell(y)
+        n_people = length(y);
+        dim_x = size(y{1}, 3);
+        correct = arrayfun(@(person_idx) isnumeric(y{person_idx}) &&...
+            isreal(y{person_idx}) && (ndims(y{person_idx}) == 3) &&...
+            all([size(y{person_idx}, 2), size(y{person_idx}, 3)] == [2, dim_x]),...
+            1:n_people);
+        if ~all(correct)
+            err_msg = ['each element in y must be a [n 2 m+1] for some n',...
+                ' and constant m'];
+            error(err_msg)
+        end
+    else
+        dim_x = size(y, 3);
+    end
+%     zero_probs must have same form as y; if y is real [n 2 m+1] then
+%     zero_probs must be [n m], if y is [n_people 1] cell then zero_probs
+%     must be [n_people 1] cell with each element [n m] for same n as
+%     corresponding element of y, and either be binary or logical
+    if iscell(y)
+        if ~(iscell(zero_probs) && iscolumn(zero_probs))
+            error('zero_probs must be a [n_people 1] cell array')
+        end
+        correct_size = arrayfun(@(person_idx) ismatrix(zero_probs{person_idx}) &&...
+            all([size(zero_probs{person_idx}, 1), size(zero_probs{person_idx}, 2)] ==...
+            [size(y{person_idx}, 1), size(y{person_idx}, 3) - 1]), 1:n_people);
+        correct_type = arrayfun(@(person_idx) islogical(zero_probs{person_idx}) ||...
+            (isnumeric(zero_probs{person_idx}) &&...
+            all((zero_probs{person_idx}(:) == 0) | (zero_probs{person_idx} == 1))),...
+            1:n_people);
+        if ~all(correct_size & correct_type)
+            err_msg = ['zero_probs must be a [n_people 1] cell array, ',...
+                'each element of which must be [n m] with n same as',...
+                ' corresponding element of y and m constant, and must',...
+                ' either be logical or binary'];
+            error(err_msg)
+        end
+    else
+        if ~(ismatrix(zero_probs) && (islogical(zero_probs) ||...
+                (isnumeric(zero_probs) &&...
+                all((zero_probs(:) == 0) | (zero_probs(:) == 1)))))
+            error('zero_probs must be [n m] and either be logical or binary')
+        end
     end
 %     if input, alpha must be a positive real, if not input it is set to 1
-    if nargin == 2
+    if nargin == 3
         if ~(isnumeric(alpha) && isreal(alpha) && isscalar(alpha) &&...
                 (alpha > 0))
             error('alpha must be a positive real number')
@@ -58,17 +116,42 @@ function [pi_1, P, emission_means, emission_covs] =...
 %     *********************************************************************
 %     Main body of code. pi_1 is drawn from D(alpha.*ones(1, dim_x)), as is
 %     each row of P. emission_means has all elements drawn from N(0, 1).
-%     emission_covs is drawn Wishart_2(2, V) as described above.
+%     emission_means and emission_covs are first initialised using either
+%     initialEmissionParameters.m or initialEmissionParametersManyPeople.m.
+%     Each latent state i's emission_mean is then drawn from
+%     N(emission_means(:, :, i), emission_covs(:, :, i)), and
+%     emission_covs(:, :, i) is drawn from 
+%     Wishart_2(2, emission_covs(:, :, i) ./ 2). The rcond of each
+%     generated covariance matrix is checked, and if it's below rcond_tol
+%     (currently set to 1e-6), another random matrix is drawn
 %     *********************************************************************
 
+    n = 4;
     random_gammas = gamrnd(alpha, 1, (dim_x+1), dim_x);
     pi_1 = random_gammas(1, :) ./ sum(random_gammas(1, :));
     P = bsxfun(@rdivide, random_gammas(2:end, :),...
         sum(random_gammas(2:end, :), 2));
-    emission_means = randn(1, 2, dim_x);
-    emission_covs = randn(2, 2, dim_x);
-    emission_covs = cell2mat(arrayfun(@(x_idx)...
-        emission_covs(:, :, x_idx) * emission_covs(:, :, x_idx)',...
-        reshape(1:dim_x, 1, 1, dim_x), 'UniformOutput', false));
+    if iscell(y)
+        [emission_means, emission_covs] =...
+            initialEmissionParametersManyPeople(y, zero_probs);
+    else
+        [emission_means, emission_covs] =...
+            initialEmissionParameters(y, zero_probs);
+    end
+    rcond_tol = 1e-6;
+    for x_idx = 1:dim_x
+        emission_chol_cov = chol(emission_covs(:, :, x_idx));
+        emission_means(:, :, x_idx) = emission_means(:, :, x_idx) +...
+            randn(1, 2) * emission_chol_cov;
+        invertible = false;
+        while ~invertible
+            emission_cov_temp = randn(n, 2) * (emission_chol_cov ./ sqrt(n));
+            emission_cov_temp = emission_cov_temp' * emission_cov_temp;
+            if rcond(emission_cov_temp) > rcond_tol
+                invertible = true;
+                emission_covs(:, :, x_idx) = emission_cov_temp;
+            end
+        end
+    end
 
 end
